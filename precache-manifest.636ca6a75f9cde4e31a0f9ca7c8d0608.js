af68ccbcb09082cc225ad40ce6abc5bdd9176a71
IliadSt <49768352+IliadSt@users.noreply.github.com>
2019-04-20 23:21:34 +0300

    Полностью скомпилированный рабочий билд на реакте.

---
+++ precache-manifest.636ca6a75f9cde4e31a0f9ca7c8d0608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa3d13ea74cf3678c5ee",
+    "url": "/static/js/main.884fecb4.chunk.js"
+  },
+  {
+    "revision": "3cc760f20e8476f1fbef",
+    "url": "/static/js/2.2ce55b12.chunk.js"
+  },
+  {
+    "revision": "fa3d13ea74cf3678c5ee",
+    "url": "/static/css/main.467eb14b.chunk.css"
+  },
+  {
+    "revision": "14eec0c10f4431e9355d2a174d2fa3ec",
+    "url": "/index.html"
+  }
+];
