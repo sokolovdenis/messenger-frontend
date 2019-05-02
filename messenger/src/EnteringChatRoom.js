4400f587ce9c4783f3e18902ff812e9699400c51
morell5 <d-9797@mail.ru>
2019-05-02 15:14:15 +0200

    assign to this fails.

--- messenger/src/EnteringChatRoom.js
+++ messenger/src/EnteringChatRoom.js
@@ -1,10 +1,14 @@
 const axios = require('axios')
 const react = require('react')
 
+const api = 'http://messenger.westeurope.cloudapp.azure.com/api/authentication/'
+
 // Класс процедуры регистрации
 class EnteringChatRoom extends react.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.token = null;
+        this.tokenExpire = null;
     }
 
     // TODO: повесить callback на кнопку
@@ -12,7 +16,7 @@ class EnteringChatRoom extends react.Component {
     signUp(login, password, name) {
         axios({
             method: 'post',
-            url: 'http://messenger.westeurope.cloudapp.azure.com/api/authentication/signup',
+            url: api + 'signup',
             headers: {
                 'content-type': 'application/json',
             },
@@ -23,19 +27,46 @@ class EnteringChatRoom extends react.Component {
             }
         }).then(function (response) {
             response = JSON.parse(response)
-            return {
-                token: response.token,
-                tokenExpire: response.expires
-            }
+            console.log(`SignUp!`);
+            this.token = response.data.token;
+            this.tokenExpire = response.data.expires;
         }).catch(function (error) {
             console.log(`Error: ${error.response.status} : ${error.response.data}`)
         });
     }
 
-    signIn() {
+    signIn(login, password) {
+        return axios({
+            method: 'post',
+            url: api + 'signin',
+            headers: {
+                'content-type': 'application/json',
+            },
+            data: {
+                login: login,
+                password: password
+            }
+        }).then(function (response) {
+            console.log(`SignIn!`);
+            // TODO: исправить падение
+            this.token = 1;
+            let x = response.data.token;
+            this.token = x;
+            this.tokenExpire = response.data.expires;
+        }).catch(function (error) {
+            console.log(`Error: ${error.response.status} : ${error.response.data}`)
+        });
+    }
 
+    getToken() {
+        return this.token;
     }
 }
 
+
 let entering = new EnteringChatRoom()
-entering.signUp("134", "567", "890")
+let login = 'ggggg'
+
+//entering.signUp(login, login, login)
+entering.signIn(login, login)
+console.log(entering.getToken())
